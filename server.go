afda7423796d7340e61c31edd6f4ac95b8914245
Watchrapon <imgo.2nd@gmail.com>
2021-03-06 14:34:43 +0700

    add hello3

--- server.go
+++ server.go
@@ -13,10 +13,16 @@ func helloHandler(c echo.Context) error {
 		"message": "hello",
 	})
 }
+func helloHandler2(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "hello",
+	})
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/hello", helloHandler)
+	e.GET("/hello3", helloHandler2)
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
 	e.Start(":" + port)
