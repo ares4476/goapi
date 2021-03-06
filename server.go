e5bdda7a767f4bd10d8f17da7b2b8654ad014a29
Watchrapon <imgo.2nd@gmail.com>
2021-03-06 15:19:55 +0700

    todo func

--- server.go
+++ server.go
@@ -6,23 +6,40 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+type todo struct {
+	ID     int    `JSON:"id"`
+	Title  string `JSON:"title"`
+	Status string `JSON:"status"`
+}
+
+var totos = map[int]*todo{
+	1: &todo{ID: 1, Title: "pay phone bill", Status: "active"},
+}
+
+func todoHandler(c echo.Context) error {
+	items := []*todo{}
+	for _, item := range totos {
+		items = append(items, item)
+	}
+
+	return c.JSON(http.StatusOK, items)
+}
 
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"message": "watchrapon",
 	})
 }
-func helloHandler2(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "hello333344444",
-	})
-}
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 	e.GET("/hello", helloHandler)
-	e.GET("/hello3", helloHandler2)
+	e.GET("/todos", todoHandler)
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
 	e.Start(":" + port)
